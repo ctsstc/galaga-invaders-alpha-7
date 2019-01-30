73daf3168aad9bc87d23050d4bd16add1df4c182
KozecM <mkozec1@sheffield.ac.uk>
2019-01-30 12:10:47 -0800

    Add main.rb. References #5.

---
+++ main.rb
@@ -0,0 +1,22 @@
+class GalagaInvaders
+
+  def initialize
+  end
+
+  def update
+  end
+
+  def draw
+  end
+
+  def show
+    update
+    while (true)
+      update
+      draw
+    end
+  end
+
+end
+
+
