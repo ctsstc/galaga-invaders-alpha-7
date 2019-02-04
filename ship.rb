7d506bd3c166f88273d59c725548db891b97544e
Cody Swartz <ctsstc@gmail.com>
2019-02-04 11:29:20 -0800

    Add shoot, move_left, move_right for Ship
    References: #10

--- ship.rb
+++ ship.rb
@@ -1,6 +1,6 @@
 class Ship
   attr_accessor(:x, :y, :height, :width, :sprite)
-  
+
   def initialize(x, y, height, width, sprite)
     @x = x
     @y = y
@@ -8,4 +8,24 @@ class Ship
     @width = width
     @sprite = sprite
   end
+
+  def shoot()
+    # TODO: make sure this matches up with Missile Implementation when added
+    new Missile.new(@x, 1)
+  end
+
+  def move_left()
+    move(-1)
+  end
+
+  def move_right()
+    move(1)
+  end
+
+  private
+
+  def move(amount)
+    @x += amount
+  end
+
 end
