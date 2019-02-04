27c3925b08b599630268fb0ca098f53902da3c5f
unknown <michael77commins@gmail.com>
2019-02-04 11:49:11 -0800

    07: Stub out Missile class

--- missile.rb
+++ missile.rb
@@ -1,12 +1,13 @@
 class Missile
 
-  def initialize(x, y, width, height, sprite, velocity)
+  def initialize(x, y, width, height, sprite, x_velocity, y_velocity)
 	  @x = x
 	  @y = y
 	  @height = height
-      @width = width
-      @sprite = sprite
-	  @velocity = velocity
+    @width = width
+    @sprite = sprite
+	  @x_velocity = x_velocity
+    @y_velocity = y_velocity
   end
 
   def draw
