466aeec8a315f1b2f5189ec691c5d4565a022885
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-02-04 11:02:55 -0800

    Stub space ship integration in main.

--- main.rb
+++ main.rb
@@ -1,21 +1,32 @@
+require_relative 'ship'
+
+
 class GalagaInvaders
 
   def initialize
     @caption = "Galaga Invaders"
+    @ship = Ship.new
   end
 
+
   def update
-    # ...
+    # aliens are moving.
   end
 
   def draw
-    # ...
+    @ship.draw
   end
 
   def show
     while (true) do
       update
       draw
+    end
+  end
+
+  def key_pressed(key)
+    if key == 'a'
+      @ship.x -= 10
     end
   end
 
