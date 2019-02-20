e28c106bccf8ca4211e33f66fec18bc4568eb96a
KozecM <mkozec1@sheffield.ac.uk>
2019-02-20 11:14:50 -0800

    19: added Enemy class with muzzle and to_s

---
+++ enemy.rb
@@ -0,0 +1,36 @@
+class Enemy
+
+  WIDTH = 50
+  HEIGHT = 50
+
+  attr_accessor :x, :y
+
+  def initialize
+    @x = 200
+    @y = 200
+  end
+
+  def move
+  end
+
+  def draw
+    puts self
+  end
+
+  def to_s
+    "👾 (#{x}, #{y})"
+  end
+
+  def ordnance_velocity
+    10
+  end
+  
+  def muzzle_location
+    [x, bottom_edge]
+  end
+  
+  def bottom_edge
+    y + HEIGHT / 2
+  end
+
+end
