a6b6acbb018c6852ce61a26ca93e81607135fb0f
Cody Swartz <ctsstc@gmail.com>
2019-02-20 11:39:18 -0800

    Adds MissileManager, Vector
    Updates ship, missiles, and draws both

--- main.rb
+++ main.rb
@@ -5,19 +5,22 @@ class GalagaInvaders
   WIDTH = 1920
   HEIGHT = 1080
 
-  attr_accessor :ship
+  attr_accessor :ship, :missiles
 
   def initialize
     @caption = "Galaga Invaders"
+    @missiles = MissileManager.new()
     @ship = Ship.new(WIDTH, HEIGHT)
   end
 
   def update
-
+    ship.update
+    missiles.update
   end
 
   def draw
     ship.draw
+    missiles.draw
   end
 
   def show
--- missile.rb
+++ missile.rb
@@ -1,23 +1,27 @@
+require_relative 'vector'
+
 class Missile
 
-  attr_accessor :x, :y
+  attr_accessor :vector
   attr_reader :sprite
 
-  def initialize(x, y, width, height, x_velocity, y_velocity)
-    @x = x
-    @y = y
-    @height = height
-    @width = width
+  def initialize(args)
+    @vector = args.fetch(:vector, Vector.new(0, 0))
+    @height = args.fetch(:height, 100)
+    @width = args.fetch(:width, 100)
     @sprite = '👾'
-    @x_velocity = x_velocity
-    @y_velocity = y_velocity
+    @velocity = args.fetch(:velocity, 0)
   end
 
   def draw
     puts self
   end
 
+  def move
+    vector.y += velocity
+  end
+
   def to_s
-    "#{sprite} (#{x}#{y})"
+    "#{sprite} (#{vector.x}#{vector.y})"
   end
 end
