0aa9de5765ff5b10595f7f1a83d78d17e41770f9
Cody Swartz <ctsstc@gmail.com>
2019-03-06 11:51:07 -0800

    Add vertical movement to Movable

--- movable.rb
+++ movable.rb
@@ -4,16 +4,28 @@ module Movable
   DEFAULT_VELOCITY = 5
 
   def move_left(amount = DEFAULT_VELOCITY)
-    move(-amount)
+    move_x(-amount)
   end
 
   def move_right(amount = DEFAULT_VELOCITY)
-    move(amount)
+    move_x(amount)
+  end
+
+  def move_up(amount = DEFAULT_VELOCITY)
+    move_y(-amount)
+  end
+
+  def move_down(amount = DEFAULT_VELOCITY)
+    move_y(amount)
   end
 
   private
 
-  def move(amount)
+  def move_x(amount)
     self.x += amount
+  end
+
+  def move_y(amount)
+    self.y += amount
   end
 end
