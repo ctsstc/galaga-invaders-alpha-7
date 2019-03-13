df928d4528a69020398c953829df5f9b24eba615
unknown <michael77commins@gmail.com>
2019-03-13 11:20:58 -0700

    Added fantasy code to main

--- main.rb
+++ main.rb
@@ -5,26 +5,52 @@ class GalagaInvaders
   WIDTH = 1920
   HEIGHT = 1080
 
-  attr_accessor :ship, :missiles, :enemy
+  attr_accessor :ship, :missiles, :squadrons, :base. :game, :wing, :updateables
 
   def initialize
     @caption = "Galaga Invaders"
-    @base
+    @game = Game.new()
+    game.start
+    @updateables = create_updateables
+  end
+
+  def create_updateables
+    @base = Base.new(WIDTH, HEIGHT)
     @missiles = MissileManager.new()
     @ship = Ship.new(WIDTH, HEIGHT)
-    @enemy = Enemy.new()
+    @squadrons
+    @wing
+    Array.new(base, missiles, ship, squadrons, wing)
   end
 
   def update
-    ship.update
-    missiles.update
+    if game.new_level?
+      game.next_level()
+    end
+    if game.time_for_next_squadron?
+      squadrons.new_squadron(game.next_pattern)
+    end
+    if game.time_for_ufo?
+      updateables << Ufo.new()
+    end
+    updateables.each(&:update)
+    if squadrons.in_place?
+      wing.add_in_place_squadrons(squadrons)
+    end
+    if wing.is_complete?
+      wing.attack
+    elsif wing.all_destroyed?
+      game.level_finished
+    end
+    game.tick
+    collisions.check(ship, missiles, squadrons, wing, base)
   end
 
   def draw
     ship.draw
     missiles.draw
-    enemy.draw
-    #draw forts (tbd)
+    squadrons.draw
+    base.draw
   end
 
   def show
